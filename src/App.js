1337586f525d3aee024ab07a723a899808b705b5
TiffPro <tiffany.guan@griffithuni.edu.au>
2020-09-29 22:51:07 +1000

    Added components for router, switch and showing navbar plus route to landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import {Router, Switch, Route} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import history from './Components/AppHistory';
+import LandingPage from './Components/LandingPage';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+      <Router history={history}>
+        <Navbar>
+          <Navbar.Brand>THE PAGE</Navbar.Brand>
+          <Navbar.Collapse>
+          <Navbar.Text>Option 1</Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+        
+        <Switch>
+          <Route exact path='/' component={LandingPage} />
+        </Switch>
+        
+        </Router>
     </div>
   );
 }
